563a2d007fd8b612bd1d7cccd15591be2496f968
foyzullah <foyz.cse@gmail.com>
2022-12-28 16:35:30 +0600

    class-15

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+<template>
+  <div class="container mx-auto">
+    <nav class="flex flex-wrap justify-center py-3 font-semibold uppercase bg-lime-800 text-white text-base">
+      <router-link to="/" class="mr-2 ">Home</router-link>
+      <router-link to="/about">About</router-link>
+
+    </nav>
+    <div class="text-center mt-3">
+
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container mx-auto">
+      <h2 class="text-lg font-bold text-center py-5">Meal Categories</h2>
+      <div class="flex flex-wrap mx-auto justify-center">
+      <div v-for="meal in meals" class="border border-orange-300 rounded-sm p-4 mb-5 text-center h-full w-1/4 mr-2">
+        <img :src="meal.strCategoryThumb" alt="">
+        <h3 class="font-semibold mt-3">{{ meal.strCategory }}</h3>
+      </div>
+    </div>
+    </div>
+  </template>
+  
+  <script>
+    export default{
+  
+      name: 'Home',
+      data(){
+        return {
+         meals:[] 
+        }
+      },
+      mounted(){
+        fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+        .then(response=> response.json())
+        .then(json=>this.meals=json.categories)
+      }
+    }
+  
+  </script>
